7b13832a8948ecee21aa1aac7556d16adbb7e2bd
krispyensign <futralj@gmail.com>
2025-06-09 02:04:58 -0400

    add oanda type

--- src/mutantsolver.adb
+++ src/mutantsolver.adb
@@ -15,10 +15,20 @@ use GNATCOLL;
 
 procedure Mutantsolver is
    package Log renames Simple_Logging;
+
    Result        : constant TOML.Read_Result :=
      TOML.File_IO.Load_File ("local_config.toml");
    Data          : Response.Data;
    Local_Headers : Headers.List;
+
+   type Oanda_Access is record
+      Token      : String (1 .. 80);
+      Account_ID : String (1 .. 80);
+      URL        : String (1 .. 256);
+   end record;
+
+   Oanda : Oanda_Access;
+
 begin
    if Result.Success then
       Text_IO.Put_Line ("config.toml loaded with success!");
@@ -27,17 +37,12 @@ begin
       Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Result.Message));
    end if;
 
-   declare
-      Oanda_Token      : constant String :=
-        Result.Value.Get ("oanda").Get ("token").As_String;
-      Oanda_Account_ID : constant String :=
-        Result.Value.Get ("oanda").Get ("account_id").As_String;
-      Oanda_URL        : constant String :=
-        Result.Value.Get ("oanda").Get ("url").As_String;
-   begin
-      Local_Headers.Add ("Bearer", Oanda_Token);
-      Data := Client.Get (URL => Oanda_URL & "/v3/instruments");
-      Text_IO.Put (Response.Message_Body (Data));
-      Log.Info ("Hello World!");
-   end;
+   Oanda :=
+     (Token      => Result.Value.Get ("oanda").Get ("token").As_String,
+      Account_ID => Result.Value.Get ("oanda").Get ("account_id").As_String,
+      URL        => Result.Value.Get ("oanda").Get ("url").As_String);
+   Local_Headers.Add ("Bearer", Oanda.Token);
+   Data := Client.Get (URL => Oanda.URL & "/v3/instruments");
+   Text_IO.Put (Response.Message_Body (Data));
+   Log.Info ("Hello World!");
 end Mutantsolver;
