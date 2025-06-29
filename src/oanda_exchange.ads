027fec749bf035ba2440c0768ebcf9942f2defb6
krispyensign <futralj@gmail.com>
2025-06-29 14:42:42 -0400

    move to private functions and formatting

--- src/oanda_exchange.ads
+++ src/oanda_exchange.ads
@@ -8,18 +8,20 @@ package Oanda_Exchange is
    package calendar renames Ada.Calendar;
    package json renames GNATCOLL.JSON;
 
+   function Fetch_Candles
+     (oanda : Config.Oanda_Access; chart : Config.Chart_Config)
+      return Core.Candles
+   with
+     Pre  => chart.Offline_Set_Size + chart.Online_Set_Size <= 5000,
+     Post => Fetch_Candles'Result'Length > 0;
+
+private
+   function Fetch_Candle_Data
+     (token : String; constructed_url : String) return json.JSON_Array;
+
    function Construct_URL
      (oanda : Config.Oanda_Access; chart : Config.Chart_Config) return String;
 
    function Make_Candle (current_candle : json.JSON_Value) return Core.Candle;
 
-   function Fetch_Candles
-     (oanda : Config.Oanda_Access; chart : Config.Chart_Config)
-      return Core.Candles
-   with
-   Pre => chart.Offline_Set_Size + chart.Online_Set_Size <= 5000,
-   Post => Fetch_Candles'Result'Length > 0;
-
-   function Fetch_Candle_Data
-     (token : String; constructed_url : String) return json.JSON_Array;
 end Oanda_Exchange;
